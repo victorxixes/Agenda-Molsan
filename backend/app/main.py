573d5e073548763b36aa1ae507a7a6bcfc735f8e
victorxixes <victor.tomas.c@gmail.com>
2026-08-10 21:22:32 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,8 +32,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://agenda-intranet-f.onrender.com",   # 🔥 TU FRONTEND ACTUAL
-        "https://agenda-intranet-front.onrender.com",
-        "https://agenda-intranet-front-v3.onrender.com",
         "http://localhost:5173"
     ],
     allow_credentials=True,

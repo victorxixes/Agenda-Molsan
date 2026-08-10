a8e35f5626a630568a0059c351370243774dbcd4
victorxixes <victor.tomas.c@gmail.com>
2026-08-10 18:35:29 +0200

    Delete frontend/realtime directory

--- frontend/realtime/notificaciones.js
+++
@@ -1,5 +0,0 @@
-import { connectWS } from "./ws";
-
-export function connectNotificaciones(usuarioId, onMessage) {
-  return connectWS(`${import.meta.env.VITE_API_URL_WS}/ws/notificaciones/${usuarioId}`, onMessage);
-}
--- frontend/realtime/seguridad.js
+++
@@ -1,7 +0,0 @@
-import { connectWS } from "./ws";
-
-export function connectSeguridad(onMessage) {
-  const WS = import.meta.env.VITE_WS_URL;   // ✔ variable correcta
-  console.log("WS URL USADA:", WS);
-  return connectWS(`${WS}/ws/seguridad`, onMessage);
-}

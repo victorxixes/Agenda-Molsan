8f8af158dbc6f8f4953aefaf75c1c0a3b0abafe4
victorxixes <victor.tomas.c@gmail.com>
2026-08-11 14:34:43 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,6 @@ app.add_middleware(
 # IMPORTAR ROUTERS
 # ---------------------------------------------------------
 from app.seguridad.router_fix_schema import fix_empleados_schema
-fix_empleados_schema()
 
 from app.seguridad.router import router as seguridad_router
 from app.seguridad.permisos_router import router as permisos_router

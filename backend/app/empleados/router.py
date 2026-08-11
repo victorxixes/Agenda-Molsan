879b26a6b869de5d913b5f12a3fd91aef822dcb9
victorxixes <victor.tomas.c@gmail.com>
2026-08-11 12:35:55 +0200

    Update router.py

--- backend/app/empleados/router.py
+++ backend/app/empleados/router.py
@@ -1,3 +1,4 @@
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.empleados.schemas import EmpleadoCreate, EmpleadoUpdate, EmpleadoResponse

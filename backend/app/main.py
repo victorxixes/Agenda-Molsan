54332cfbd5f342c1e9ea693f31a8acb7a9fb8ced
victorxixes <victor.tomas.c@gmail.com>
2026-08-12 10:00:37 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -125,6 +125,7 @@ app.include_router(force_router)
 # ---------------------------------------------------------
 # STATIC FILES
 # ---------------------------------------------------------
-STATIC_DIR = os.path.join(os.path.dirname(__file__), "../static")
+STATIC_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../static"))
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
 

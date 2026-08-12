5beff60314c824e7e9e0986e3bec3e1bca9cfe91
victorxixes <victor.tomas.c@gmail.com>
2026-08-12 22:25:36 +0200

    Update router.py

--- backend/app/auditoria/router.py
+++ backend/app/auditoria/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.database import get_db
 from datetime import datetime
+
+from backend.app.database import get_db
 
 router = APIRouter(prefix="/auditoria", tags=["Auditoria"])
 

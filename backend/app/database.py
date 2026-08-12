8f00c995c36b3bb0339bfc863188c19168d32dbe
victorxixes <victor.tomas.c@gmail.com>
2026-08-12 22:22:51 +0200

    Fix import path for settings in database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.config import settings
+
+from backend.app.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 

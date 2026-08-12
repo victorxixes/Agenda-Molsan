931faa2fbafef54329663d9f5978e19da0bd3dd4
victorxixes <victor.tomas.c@gmail.com>
2026-08-12 12:53:08 +0200

    Update models.py

--- backend/app/maestros/models.py
+++ backend/app/maestros/models.py
@@ -3,23 +3,18 @@ from app.database import Base
 
 class Departamento(Base):
     __tablename__ = "departamentos"
-
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
     descripcion = Column(String, nullable=True)
-
 
 class Seccion(Base):
     __tablename__ = "secciones"
-
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
     descripcion = Column(String, nullable=True)
 
-
 class Cargo(Base):
     __tablename__ = "cargos"
-
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
     descripcion = Column(String, nullable=True)

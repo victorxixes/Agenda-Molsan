39a760a89e23c1838e4cef1987bec9cf119374ab
victorxixes <victor.tomas.c@gmail.com>
2026-08-12 13:17:33 +0200

    Update models.py

--- backend/app/maestros/models.py
+++ backend/app/maestros/models.py
@@ -7,14 +7,20 @@ class Departamento(Base):
     nombre = Column(String, unique=True, nullable=False)
     descripcion = Column(String, nullable=True)
 
+    empleados = relationship("Empleado", back_populates="departamento")
+
 class Seccion(Base):
     __tablename__ = "secciones"
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
     descripcion = Column(String, nullable=True)
 
+    empleados = relationship("Empleado", back_populates="seccion")
+
 class Cargo(Base):
     __tablename__ = "cargos"
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
     descripcion = Column(String, nullable=True)
+
+    empleados = relationship("Empleado", back_populates="cargo")

aa594150a50cb27a37d6c5f5a30a8a6b01b4c138
victorxixes <victor.tomas.c@gmail.com>
2026-08-12 12:10:24 +0200

    Update models.py

--- backend/app/empleados/models.py
+++ backend/app/empleados/models.py
@@ -7,6 +7,38 @@ class Empleado(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
+    # ---------------------------------------------------------
+# TABLAS MAESTRAS
+# ---------------------------------------------------------
+
+class Departamento(Base):
+    __tablename__ = "departamentos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, nullable=False)
+    descripcion = Column(String, nullable=True)
+
+    empleados = relationship("Empleado", back_populates="departamento")
+
+
+class Seccion(Base):
+    __tablename__ = "secciones"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, nullable=False)
+    descripcion = Column(String, nullable=True)
+
+    empleados = relationship("Empleado", back_populates="seccion")
+
+
+class Cargo(Base):
+    __tablename__ = "cargos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, nullable=False)
+    descripcion = Column(String, nullable=True)
+
+    empleados = relationship("Empleado", back_populates="cargo")
     # -----------------------------
     # DATOS PERSONALES
     # -----------------------------

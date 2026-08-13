bc7af4b220c31b97eec27d8448f50b97be19b340
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 07:56:40 +0200

    Update router_force.py

--- backend/app/utilidades/router_force.py
+++ backend/app/utilidades/router_force.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import text
-from app.database import get_db
+
+from backend.app.database import get_db
 
 router = APIRouter(prefix="/force", tags=["Force"])
 

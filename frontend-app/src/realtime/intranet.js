58535ab90fc52724fb8e8d22952f9bdbbc2345dd
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 18:02:46 +0200

    Update intranet.js

--- frontend-app/src/realtime/intranet.js
+++ frontend-app/src/realtime/intranet.js
@@ -1,8 +1,17 @@
 import { connectWS } from "./ws";
 
-export function connectIntranet(onMessage) {
-  return connectWS(
+export function connectIntranet(usuarioId, onMessage) {
+  const ws = connectWS(
     `${import.meta.env.VITE_API_URL_WS}/ws/intranet`,
     onMessage
   );
+
+  // Enviar ID del usuario al conectar
+  setTimeout(() => {
+    try {
+      ws.socket?.send(JSON.stringify({ usuario_id: usuarioId }));
+    } catch {}
+  }, 300);
+
+  return ws;
 }

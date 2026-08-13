7969916c64bc304db692f4c2d4d53331308d4dc4
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 08:02:19 +0200

    Update models.py

--- backend/app/maestros/models.py
+++ backend/app/maestros/models.py
@@ -1,25 +1,35 @@
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
-from app.database import Base
+
+from backend.app.database import Base
+
 
 class Departamento(Base):
     __tablename__ = "departamentos"
+    __allow_unmapped__ = True
+
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
     descripcion = Column(String, nullable=True)
 
     empleados = relationship("Empleado", back_populates="departamento")
 
+
 class Seccion(Base):
     __tablename__ = "secciones"
+    __allow_unmapped__ = True
+
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
     descripcion = Column(String, nullable=True)
 
     empleados = relationship("Empleado", back_populates="seccion")
 
+
 class Cargo(Base):
     __tablename__ = "cargos"
+    __allow_unmapped__ = True
+
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, nullable=False)
     descripcion = Column(String, nullable=True)

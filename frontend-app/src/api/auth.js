9f6f71c1e8711723f126e1bd8832cd446fffe85f
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 21:45:58 +0200

    Update auth.js

--- frontend-app/src/api/auth.js
+++ frontend-app/src/api/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const loginRequest = async (usuario, password) => {
   const res = await axios.post(
-    "https://agenda-intranet-backend.onrender.com/empleados/login",
+    "https://agenda-intranet-b.onrender.com/empleados/login",
     {
       usuario,
       password

9db83d72f20b0bdeab9019f9e6b0346379f328e0
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 14:41:51 +0200

    Update models.py

--- backend/app/agenda/models.py
+++ backend/app/agenda/models.py
@@ -5,19 +5,20 @@ class Cita(Base):
     __tablename__ = "agenda_citas"
     __allow_unmapped__ = True
 
+class Cita(Base):
+    __tablename__ = "agenda_citas"
+
     id = Column(Integer, primary_key=True, index=True)
 
     fecha = Column(Date, nullable=False)
     hora_inicio = Column(Time, nullable=False)
     hora_fin = Column(Time, nullable=False)
+    tipo_cita = Column(String, nullable=False)
 
-    tipo_cita = Column(String, nullable=False, default="Otros")
-
+    # Campos opcionales (sin FK)
     notario_id = Column(Integer, nullable=True)
-
     tipo_firma = Column(String, nullable=True)
-
     apoderado_id = Column(Integer, nullable=True)
-
-    estado = Column(String, nullable=False, default="Pendiente")
     observaciones = Column(String, nullable=True)
+
+    estado = Column(String, default="Pendiente")

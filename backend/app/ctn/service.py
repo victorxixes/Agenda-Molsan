1ee5ad5429e06e40b1775497ef3f46db65704994
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 08:24:18 +0200

    Update service.py

--- backend/app/ctn/service.py
+++ backend/app/ctn/service.py
@@ -1,12 +1,16 @@
 from sqlalchemy.orm import Session
-from app.ctn.models import Notaria
-from app.ctn.schemas import NotariaCreate
+
+from backend.app.ctn.models import Notaria
+from backend.app.ctn.schemas import NotariaCreate
+
 
 def listar_notarias(db: Session):
     return db.query(Notaria).all()
 
+
 def obtener_notaria(db: Session, notaria_id: int):
     return db.query(Notaria).filter(Notaria.id == notaria_id).first()
+
 
 def crear_notaria(db: Session, data: NotariaCreate):
     notaria = Notaria(**data.dict())
@@ -15,18 +19,25 @@ def crear_notaria(db: Session, data: NotariaCreate):
     db.refresh(notaria)
     return notaria
 
+
 def actualizar_notaria(db: Session, notaria_id: int, data: NotariaCreate):
     notaria = obtener_notaria(db, notaria_id)
-    if notaria:
-        for k, v in data.dict().items():
-            setattr(notaria, k, v)
-        db.commit()
-        db.refresh(notaria)
+    if not notaria:
+        return None
+
+    for k, v in data.dict().items():
+        setattr(notaria, k, v)
+
+    db.commit()
+    db.refresh(notaria)
     return notaria
+
 
 def eliminar_notaria(db: Session, notaria_id: int):
     notaria = obtener_notaria(db, notaria_id)
-    if notaria:
-        db.delete(notaria)
-        db.commit()
+    if not notaria:
+        return None
+
+    db.delete(notaria)
+    db.commit()
     return notaria

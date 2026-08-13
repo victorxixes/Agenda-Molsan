e23465041fab92d1d81b66811d7b8aedf567995b
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 21:54:23 +0200

    Update logsStore.js

--- frontend-app/src/store/logsStore.js
+++ frontend-app/src/store/logsStore.js
@@ -11,13 +11,12 @@ export const useLogsStore = create((set) => ({
     set({ logs: data, loading: false });
   },
 
-filtrarPorUsuario: async (id) => {
-  if (!id || isNaN(id)) return;   // 🔥 evita 422
-  set({ loading: true });
-  const data = await logsAPI.porUsuario(id);
-  set({ logs: data, loading: false });
-},
-
+  filtrarPorUsuario: async (id) => {
+    if (!id || isNaN(id)) return;   // 🔥 evita 422
+    set({ loading: true });
+    const data = await logsAPI.porUsuario(id);
+    set({ logs: data, loading: false });
+  },
 
   filtrarPorModulo: async (modulo) => {
     set({ loading: true });

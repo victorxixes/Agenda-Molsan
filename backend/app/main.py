32f8f9025be121957b22a0dddfd0d5b3ae22bd62
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 15:02:33 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,6 @@
+from app. agenda.fix_table import fix_table
+fix_table()
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles

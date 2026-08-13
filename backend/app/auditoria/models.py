ca18c3abdeb8638fb1d799cc63dad533b03d5a12
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 08:08:45 +0200

    Update models.py

--- backend/app/auditoria/models.py
+++ backend/app/auditoria/models.py
@@ -1,14 +1,21 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
-from app.database import Base
+
+from backend.app.database import Base
+
 
 class Auditoria(Base):
     __tablename__ = "auditoria2"
+    __allow_unmapped__ = True
 
     id = Column(Integer, primary_key=True, index=True)
+
     usuario = Column(String, nullable=True)
     modulo = Column(String, nullable=True)
     accion = Column(String, nullable=True)
     descripcion = Column(String, nullable=True)
+
     fecha = Column(DateTime, default=datetime.utcnow)
-    ip = Column(String(50), nullable=True)   # ← CORRECTO
+
+    # IP del usuario o del sistema
+    ip = Column(String(50), nullable=True)

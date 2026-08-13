1e90943ae9322ffc015178b7e22d25dc2743d5ac
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 08:22:55 +0200

    Update service.py

--- backend/app/maestros/service.py
+++ backend/app/maestros/service.py
@@ -1,12 +1,15 @@
 from sqlalchemy.orm import Session
-from app.maestros.models import Departamento, Seccion, Cargo
-from app.maestros.schemas import DepartamentoCreate, SeccionCreate, CargoCreate
+
+from backend.app.maestros.models import Departamento, Seccion, Cargo
+from backend.app.maestros.schemas import DepartamentoCreate, SeccionCreate, CargoCreate
+
 
 # -------------------------
 # DEPARTAMENTOS
 # -------------------------
 def listar_departamentos(db: Session):
     return db.query(Departamento).all()
+
 
 def crear_departamento(db: Session, data: DepartamentoCreate):
     dep = Departamento(**data.dict())
@@ -15,29 +18,36 @@ def crear_departamento(db: Session, data: DepartamentoCreate):
     db.refresh(dep)
     return dep
 
+
 def editar_departamento(db: Session, id: int, data: DepartamentoCreate):
     dep = db.query(Departamento).filter(Departamento.id == id).first()
     if not dep:
         return None
+
     for k, v in data.dict().items():
         setattr(dep, k, v)
+
     db.commit()
     db.refresh(dep)
     return dep
+
 
 def eliminar_departamento(db: Session, id: int):
     dep = db.query(Departamento).filter(Departamento.id == id).first()
     if not dep:
         return False
+
     db.delete(dep)
     db.commit()
     return True
+
 
 # -------------------------
 # SECCIONES
 # -------------------------
 def listar_secciones(db: Session):
     return db.query(Seccion).all()
+
 
 def crear_seccion(db: Session, data: SeccionCreate):
     sec = Seccion(**data.dict())
@@ -46,29 +56,36 @@ def crear_seccion(db: Session, data: SeccionCreate):
     db.refresh(sec)
     return sec
 
+
 def editar_seccion(db: Session, id: int, data: SeccionCreate):
     sec = db.query(Seccion).filter(Seccion.id == id).first()
     if not sec:
         return None
+
     for k, v in data.dict().items():
         setattr(sec, k, v)
+
     db.commit()
     db.refresh(sec)
     return sec
+
 
 def eliminar_seccion(db: Session, id: int):
     sec = db.query(Seccion).filter(Seccion.id == id).first()
     if not sec:
         return False
+
     db.delete(sec)
     db.commit()
     return True
+
 
 # -------------------------
 # CARGOS
 # -------------------------
 def listar_cargos(db: Session):
     return db.query(Cargo).all()
+
 
 def crear_cargo(db: Session, data: CargoCreate):
     cargo = Cargo(**data.dict())
@@ -77,20 +94,25 @@ def crear_cargo(db: Session, data: CargoCreate):
     db.refresh(cargo)
     return cargo
 
+
 def editar_cargo(db: Session, id: int, data: CargoCreate):
     cargo = db.query(Cargo).filter(Cargo.id == id).first()
     if not cargo:
         return None
+
     for k, v in data.dict().items():
         setattr(cargo, k, v)
+
     db.commit()
     db.refresh(cargo)
     return cargo
+
 
 def eliminar_cargo(db: Session, id: int):
     cargo = db.query(Cargo).filter(Cargo.id == id).first()
     if not cargo:
         return False
+
     db.delete(cargo)
     db.commit()
     return True

426c3480257cc56dabac984fe7135e3cc4d6b1ec
victorxixes <victor.tomas.c@gmail.com>
2026-08-13 08:00:59 +0200

    Update models.py

--- backend/app/ctn/models.py
+++ backend/app/ctn/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from backend.app.database import Base
 
+
 class Notaria(Base):
     __tablename__ = "ctn_notarios"
     __allow_unmapped__ = True
@@ -27,6 +28,7 @@ class Notaria(Base):
     apoderado_s = Column(String, nullable=True)
 
     observacion = Column(String, nullable=True)
+
 
 # Alias para compatibilidad con informes
 Notario = Notaria

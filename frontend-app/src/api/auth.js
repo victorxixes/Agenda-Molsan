82e01743504846d02cdc76f255325bbb7e3e2b39
victorxixes <victor.tomas.c@gmail.com>
2026-08-14 08:24:19 +0200

    Update auth.js

--- frontend-app/src/api/auth.js
+++ frontend-app/src/api/auth.js
@@ -1,13 +1,11 @@
 import axios from "../api/axios.js";
 
 export const loginRequest = async (usuario, password) => {
-  return axios({
-    method: "POST",
-    url: "/empleados/login",
-    data: { usuario, password },
-    headers: {
-      "Content-Type": "application/json"
-    }
-  }).then(res => res.data);
+  const res = await axios.post(
+    "/auth/login",
+    { usuario, password }
+  );
+
+  return res.data;
 };
 

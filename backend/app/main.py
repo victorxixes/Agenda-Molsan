7fcd3b909d313b07f4e642b04d7315b9685cedcb
victorxixes <victor.tomas.c@gmail.com>
2026-08-14 15:39:28 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -93,7 +93,7 @@ from backend.app.utilidades.router_force import router as force_router
 # app.include_router(router_fix_pg)
 # app.include_router(router_fix_noticias)
 
-app.include_router(auth_router)
+app.include_router(auth_router, prefix="/api")
 app.include_router(seguridad_router)
 app.include_router(permisos_router)
 

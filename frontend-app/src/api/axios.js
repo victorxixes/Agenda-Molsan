d4073fd87068631112f045ea0a256491d97a437e
victorxixes <victor.tomas.c@gmail.com>
2026-08-14 15:04:10 +0200

    Update axios.js

--- frontend-app/src/api/axios.js
+++ frontend-app/src/api/axios.js
@@ -1,21 +1,14 @@
 import axios from "axios";
 
-// Base URL del backend
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-// ---------------------------------------------------------
-// INTERCEPTOR PARA AÑADIR TOKEN AUTOMÁTICAMENTE
-// ---------------------------------------------------------
+// Interceptor para añadir token excepto en login
 axios.interceptors.request.use((config) => {
-  // No añadir token en el login
-  if (config.url.includes("/auth/login")) {
+  if (config.url.includes("/api/auth/login")) {
     return config;
   }
 
-  // Leer token del localStorage
   const token = localStorage.getItem("token");
-
-  // Si existe, añadirlo
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }

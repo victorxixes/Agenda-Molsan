5a53ea59b42ab1dd4403991db4139782dcb5e427
victorxixes <victor.tomas.c@gmail.com>
2026-08-15 16:30:36 +0200

    Update dashboard.js

--- frontend-app/src/api/dashboard.js
+++ frontend-app/src/api/dashboard.js
@@ -1,11 +1,10 @@
 import axios from "axios";
+
 const API = import.meta.env.VITE_API_URL;
 
 export const dashboardAPI = {
-  resumenAgenda: (empleadoId) =>
-    axios
-      .get(`${API}/dashboard/resumen`, {
-        params: { empleado_id: empleadoId }
-      })
-      .then(r => r.data),
+  resumen: async () => {
+    const res = await axios.get(`${API}/dashboard/resumen`);
+    return res.data;
+  }
 };

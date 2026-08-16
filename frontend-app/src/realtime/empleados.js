1e7b05d593495012ca9cbec6079534f901c88095
victorxixes <victor.tomas.c@gmail.com>
2026-08-16 12:55:30 +0200

    Update empleados.js

--- frontend-app/src/realtime/empleados.js
+++ frontend-app/src/realtime/empleados.js
@@ -1,10 +1,11 @@
 import { connectWS } from "./ws";
 
 export function connectEmpleados(empleadoId, onMessage) {
+  const WS = import.meta.env.VITE_WS_URL;
+
   return connectWS(
-    `${import.meta.env.VITE_API_URL_WS}/ws/empleados`,
+    `${WS}/ws/empleados`,
     onMessage,
-    // Primer mensaje que enviamos al WS
     () => ({ empleado_id: empleadoId })
   );
 }

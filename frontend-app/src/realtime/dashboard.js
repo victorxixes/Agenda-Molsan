dbd2ecb044848b9a335db60124c4cfa98db1d079
victorxixes <victor.tomas.c@gmail.com>
2026-08-16 12:55:22 +0200

    Update dashboard.js

--- frontend-app/src/realtime/dashboard.js
+++ frontend-app/src/realtime/dashboard.js
@@ -1,16 +1,16 @@
 import { connectWS } from "./ws";
 
 export function connectDashboard(onMessage) {
+  const WS = import.meta.env.VITE_WS_URL;
+
   try {
-    return connectWS(
-      `${import.meta.env.VITE_API_URL_WS}/ws/dashboard`,
-      (msg) => {
-        if (!msg) return;
-        onMessage(msg);
-      }
-    );
+    return connectWS(`${WS}/ws/dashboard`, (msg) => {
+      if (!msg) return;
+      onMessage(msg);
+    });
   } catch (e) {
     console.error("Error conectando WS Dashboard:", e);
     return { close: () => {} };
   }
 }
+

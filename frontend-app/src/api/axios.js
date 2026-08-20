2b8301e4e9f64f1f6d7a401f36bc4750e2c27693
victorxixes <victor.tomas.c@gmail.com>
2026-08-20 20:00:19 +0200

    Update axios.js

--- frontend-app/src/api/axios.js
+++ frontend-app/src/api/axios.js
@@ -2,8 +2,13 @@ import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
+// Todas las rutas REST llevan /api
 axios.interceptors.request.use((config) => {
-  if (config.url.startsWith("/auth/login")) {
+  if (!config.url.startsWith("/api")) {
+    config.url = "/api" + config.url;
+  }
+
+  if (config.url.startsWith("/api/auth/login")) {
     return config;
   }
 

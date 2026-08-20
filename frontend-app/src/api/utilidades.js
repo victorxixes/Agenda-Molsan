cd0cb0a28eca0628bcddcc8d51f4c57d7dd23f51
victorxixes <victor.tomas.c@gmail.com>
2026-08-20 20:06:29 +0200

    Update utilidades.js

--- frontend-app/src/api/utilidades.js
+++ frontend-app/src/api/utilidades.js
@@ -4,12 +4,12 @@ export const utilidadesAPI = {
   importarCTN: (file) => {
     const form = new FormData();
     form.append("file", file);
-    return axios.post(`/utilidades/importar-ctn`, form).then(r => r.data);
+    return axios.post(`/api/utilidades/importar-ctn`, form).then(r => r.data);
   },
 
   crearNoticia: (payload) =>
-    axios.post(`/utilidades/crear-noticia`, payload).then(r => r.data),
+    axios.post(`/api/utilidades/crear-noticia`, payload).then(r => r.data),
 
   subirDocumento: (formData) =>
-    axios.post(`/utilidades/subir-documento`, formData).then(r => r.data)
+    axios.post(`/api/utilidades/subir-documento`, formData).then(r => r.data)
 };

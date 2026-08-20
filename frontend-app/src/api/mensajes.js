ac55252f617290d5c3ec5b1a38114b90290455a7
victorxixes <victor.tomas.c@gmail.com>
2026-08-20 19:19:04 +0200

    Update mensajes.js

--- frontend-app/src/api/mensajes.js
+++ frontend-app/src/api/mensajes.js
@@ -1,25 +1,24 @@
-import axios from "axios";
-const API = import.meta.env.VITE_API_URL;
+import axios from "../api/axios";
 
 export const mensajesAPI = {
   conectar: (usuarioId) =>
-    axios.post(`${API}/mensajes/conectar/${usuarioId}`).then(r => r.data),
+    axios.post(`/mensajes/conectar/${usuarioId}`).then(r => r.data),
 
   desconectar: (usuarioId) =>
-    axios.post(`${API}/mensajes/desconectar/${usuarioId}`).then(r => r.data),
+    axios.post(`/mensajes/desconectar/${usuarioId}`).then(r => r.data),
 
   conectados: () =>
-    axios.get(`${API}/mensajes/conectados`).then(r => r.data),
+    axios.get(`/mensajes/conectados`).then(r => r.data),
 
   conversacion: (u1, u2) =>
-    axios.get(`${API}/mensajes/conversacion/${u1}/${u2}`).then(r => r.data),
+    axios.get(`/mensajes/conversacion/${u1}/${u2}`).then(r => r.data),
 
   enviar: (data) =>
-    axios.post(`${API}/mensajes`, data).then(r => r.data),
+    axios.post(`/mensajes`, data).then(r => r.data),
 
   marcarLeido: (remitente, destinatario) =>
-    axios.put(`${API}/mensajes/leido/${remitente}/${destinatario}`).then(r => r.data),
+    axios.put(`/mensajes/leido/${remitente}/${destinatario}`).then(r => r.data),
 
   noLeidos: (usuarioId) =>
-    axios.get(`${API}/mensajes/no-leidos/${usuarioId}`).then(r => r.data),
+    axios.get(`/mensajes/no-leidos/${usuarioId}`).then(r => r.data),
 };

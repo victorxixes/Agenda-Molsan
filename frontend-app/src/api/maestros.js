6efb839e350c5a365d9eb94300152e937fbb5ef3
victorxixes <victor.tomas.c@gmail.com>
2026-08-20 19:18:52 +0200

    Update maestros.js

--- frontend-app/src/api/maestros.js
+++ frontend-app/src/api/maestros.js
@@ -1,19 +1,19 @@
-import axios from "axios";
-const API = import.meta.env.VITE_API_URL;
+import axios from "../api/axios";
 
 export const maestrosAPI = {
   departamentos: {
-    listar: () => axios.get(`${API}/maestros/departamentos`).then(r => r.data),
-    crear: (data) => axios.post(`${API}/maestros/departamentos`, data).then(r => r.data),
+    listar: () => axios.get(`/maestros/departamentos`).then(r => r.data),
+    crear: (data) => axios.post(`/maestros/departamentos`, data).then(r => r.data),
   },
 
   secciones: {
-    listar: () => axios.get(`${API}/maestros/secciones`).then(r => r.data),
-    crear: (data) => axios.post(`${API}/maestros/secciones`, data).then(r => r.data),
+    listar: () => axios.get(`/maestros/secciones`).then(r => r.data),
+    crear: (data) => axios.post(`/maestros/secciones`, data).then(r => r.data),
   },
 
   cargos: {
-    listar: () => axios.get(`${API}/maestros/cargos`).then(r => r.data),
-    crear: (data) => axios.post(`${API}/maestros/cargos`, data).then(r => r.data),
+    listar: () => axios.get(`/maestros/cargos`).then(r => r.data),
+    crear: (data) => axios.post(`/maestros/cargos`, data).then(r => r.data),
   },
 };
+

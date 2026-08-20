da218a67a27025b330c445051608327bc0c48ec8
victorxixes <victor.tomas.c@gmail.com>
2026-08-20 19:17:18 +0200

    Update informesApi.js

--- frontend-app/src/api/informesApi.js
+++ frontend-app/src/api/informesApi.js
@@ -1,20 +1,16 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
-});
+import axios from "../api/axios";
 
 export async function getInformeAgenda(year, month, day) {
-  const { data } = await api.get(`/informes/agenda/${year}/${month}/${day}`);
+  const { data } = await axios.get(`/informes/agenda/${year}/${month}/${day}`);
   return data;
 }
 
 export async function getInformeApoderados() {
-  const { data } = await api.get(`/informes/apoderados`);
+  const { data } = await axios.get(`/informes/apoderados`);
   return data;
 }
 
 export async function getInformeZonas() {
-  const { data } = await api.get(`/informes/zonas`);
+  const { data } = await axios.get(`/informes/zonas`);
   return data;
 }

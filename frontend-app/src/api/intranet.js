80b6c3c0828494cc4a59221fc1f79f758c9b2b78
victorxixes <victor.tomas.c@gmail.com>
2026-08-20 19:18:32 +0200

    Update intranet.js

--- frontend-app/src/api/intranet.js
+++ frontend-app/src/api/intranet.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-const API = import.meta.env.VITE_API_URL;
+import axios from "../api/axios";
 
 // API INTRANET
 export const intranetAPI = {
@@ -7,34 +6,34 @@ export const intranetAPI = {
   // NOTICIAS
   // ---------------------------------------------------------
   listarNoticias: () =>
-    axios.get(`${API}/intranet/noticias/`).then(r => r.data),
+    axios.get(`/intranet/noticias/`).then(r => r.data),
 
   crearNoticia: (data) =>
-    axios.post(`${API}/intranet/noticias/`, data).then(r => r.data),
+    axios.post(`/intranet/noticias/`, data).then(r => r.data),
 
   editarNoticia: (id, data) =>
-    axios.put(`${API}/intranet/noticias/${id}/`, data).then(r => r.data),
+    axios.put(`/intranet/noticias/${id}/`, data).then(r => r.data),
 
   eliminarNoticia: (id) =>
-    axios.delete(`${API}/intranet/noticias/${id}/`).then(r => r.data),
+    axios.delete(`/intranet/noticias/${id}/`).then(r => r.data),
 
   // ---------------------------------------------------------
   // DOCUMENTOS
   // ---------------------------------------------------------
   listarDocumentos: () =>
-    axios.get(`${API}/intranet/documentos/`).then(r => r.data),
+    axios.get(`/intranet/documentos/`).then(r => r.data),
 
   crearDocumento: (formData) =>
-    axios.post(`${API}/intranet/documentos/`, formData).then(r => r.data),
+    axios.post(`/intranet/documentos/`, formData).then(r => r.data),
 
   editarDocumento: (id, formData) =>
-    axios.put(`${API}/intranet/documentos/${id}/`, formData).then(r => r.data),
+    axios.put(`/intranet/documentos/${id}/`, formData).then(r => r.data),
 
   eliminarDocumento: (id) =>
-    axios.delete(`${API}/intranet/documentos/${id}/`).then(r => r.data),
+    axios.delete(`/intranet/documentos/${id}/`).then(r => r.data),
 
   descargarDocumento: (id) =>
-    axios.get(`${API}/intranet/documentos/${id}/descargar/`, {
+    axios.get(`/intranet/documentos/${id}/descargar/`, {
       responseType: "blob",
     }),
 };

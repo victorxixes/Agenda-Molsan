c6ac465c17054f896198a94ab95698b453ccf560
victorxixes <victor.tomas.c@gmail.com>
2026-08-20 19:18:42 +0200

    Update logs.js

--- frontend-app/src/api/logs.js
+++ frontend-app/src/api/logs.js
@@ -1,22 +1,21 @@
 import axios from "../api/axios";
-const API = import.meta.env.VITE_API_URL;
 
 export const logsAPI = {
   listar: () =>
-    axios.get(`${API}/logs`).then(r => r.data),
+    axios.get(`/logs`).then(r => r.data),
 
   crear: (data) =>
-    axios.post(`${API}/logs`, data).then(r => r.data),
+    axios.post(`/logs`, data).then(r => r.data),
 
   porUsuario: (id) =>
-    axios.get(`${API}/logs/usuario/${id}`).then(r => r.data),
+    axios.get(`/logs/usuario/${id}`).then(r => r.data),
 
   porModulo: (modulo) =>
-    axios.get(`${API}/logs/modulo/${modulo}`).then(r => r.data),
+    axios.get(`/logs/modulo/${modulo}`).then(r => r.data),
 
   porNivel: (nivel) =>
-    axios.get(`${API}/logs/nivel/${nivel}`).then(r => r.data),
+    axios.get(`/logs/nivel/${nivel}`).then(r => r.data),
 
   porFecha: (fechaISO) =>
-    axios.get(`${API}/logs/fecha/${fechaISO}`).then(r => r.data),
+    axios.get(`/logs/fecha/${fechaISO}`).then(r => r.data),
 };

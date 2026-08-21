cfb585763fa84a57ffd6855aad4a96da2c47c610
victorxixes <victor.tomas.c@gmail.com>
2026-08-21 09:56:37 +0200

    Create dashboardAgenda.js

---
+++ frontend-app/src/api/dashboardAgenda.js
@@ -0,0 +1,8 @@
+import axios from "../api/axios";
+
+export const dashboardAgendaAPI = {
+  obtenerKPIs: async () => {
+    const res = await axios.get("/dashboard/agenda/kpis");
+    return res.data;
+  }
+};

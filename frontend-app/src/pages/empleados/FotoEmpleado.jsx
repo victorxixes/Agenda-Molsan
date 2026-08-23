e2da23cc10d9b0f19206b10d66e6d56c691012bf
victorxixes <victor.tomas.c@gmail.com>
2026-08-23 10:35:35 +0200

    Update FotoEmpleado.jsx

--- frontend-app/src/pages/empleados/FotoEmpleado.jsx
+++ frontend-app/src/pages/empleados/FotoEmpleado.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "../api/axios";
+import axios from "../../api/axios";
 
 export default function FotoEmpleado({ empleadoId }) {
   const [empleado, setEmpleado] = useState(null);

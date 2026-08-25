92bb3c7b5515a292b9ebfebe5245bd04803e9e55
victorxixes <victor.tomas.c@gmail.com>
2026-08-25 20:57:35 +0200

    Update seguridad.js

--- frontend-app/src/api/seguridad.js
+++ frontend-app/src/api/seguridad.js
@@ -1,24 +1,18 @@
 import axios from "./axios";
 
 export const seguridadAPI = {
-
-  // 🔥 Listar roles reales
   listarRoles: () =>
     axios.get(`/seguridad/roles`).then(r => r.data),
 
-  // 🔥 Crear rol
   crearRol: (data) =>
     axios.post(`/seguridad/roles`, data).then(r => r.data),
 
-  // 🔥 Obtener permisos base
   obtenerPermisosBase: () =>
     axios.get(`/seguridad/permisos`).then(r => r.data),
 
-  // 🔥 Obtener permisos por rol
   obtenerPermisosRol: (rolId) =>
     axios.get(`/seguridad/permisos/${rolId}`).then(r => r.data),
 
-  // 🔥 Eventos de seguridad
   listarEventos: () =>
     axios.get(`/seguridad/eventos`).then(r => r.data),
 };

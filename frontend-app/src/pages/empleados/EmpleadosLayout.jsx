f8186ff7f6f3691e2368f75f3dfa8d557c80abfa
victorxixes <victor.tomas.c@gmail.com>
2026-08-26 09:37:30 +0200

    Update EmpleadosLayout.jsx

--- frontend-app/src/pages/empleados/EmpleadosLayout.jsx
+++ frontend-app/src/pages/empleados/EmpleadosLayout.jsx
@@ -1,11 +1,11 @@
-import EmpleadosPage from "./EmpleadosPage";
+import EmpleadosList from "./EmpleadosList";
 import EmpleadosRealtimePanel from "../../components/empleados/EmpleadosRealtimePanel";
 
 export default function EmpleadosLayout() {
   return (
     <div className="p-6 space-y-10">
-      <EmpleadosPage />
       <EmpleadosRealtimePanel />
+      <EmpleadosList />
     </div>
   );
 }

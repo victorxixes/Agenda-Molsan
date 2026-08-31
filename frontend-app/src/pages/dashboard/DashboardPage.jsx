00d975bbb34dde1ea11b99d43f57989f361a2c6b
victorxixes <victor.tomas.c@gmail.com>
2026-08-31 09:54:16 +0200

    Update DashboardPage.jsx

--- frontend-app/src/pages/dashboard/DashboardPage.jsx
+++ frontend-app/src/pages/dashboard/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "../api/axios";
+import axios from "../../api/axios";
 
 export default function DashboardPage() {
   const [data, setData] = useState(null);

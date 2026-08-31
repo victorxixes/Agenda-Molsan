bb959341aff51527959c4107f1a58e8f8a19d48c
victorxixes <victor.tomas.c@gmail.com>
2026-08-31 18:57:33 +0200

    Update getFotoURL.js

--- frontend-app/src/HELPERS/getFotoURL.js
+++ frontend-app/src/HELPERS/getFotoURL.js
@@ -1,1 +1,8 @@
+export const getFotoURL = (foto) => {
+  if (!foto || foto.trim() === "") {
+    return `${import.meta.env.VITE_API_URL}/api/fotos/default.jpg`;
+  }
+
+  return `${import.meta.env.VITE_API_URL}/api/fotos/${foto}`;
+};
 

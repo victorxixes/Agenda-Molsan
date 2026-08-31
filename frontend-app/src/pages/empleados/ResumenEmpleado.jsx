f2cf6f234046f71b8d3234ddc65b9fb922cee11e
victorxixes <victor.tomas.c@gmail.com>
2026-08-31 16:12:59 +0200

    Update ResumenEmpleado.jsx

--- frontend-app/src/pages/empleados/ResumenEmpleado.jsx
+++ frontend-app/src/pages/empleados/ResumenEmpleado.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { useEmpleadosStore } from "../../store/empleadosStore";
 
 const getFotoURL = (foto) => {
-  if (!foto || foto === "string" || foto.trim() === "") {
-    return "/placeholder.png";
+  if (!foto || foto.trim() === "") {
+    return `${import.meta.env.VITE_API_URL}/fotos/default-avatar.png`;
   }
-  if (foto.startsWith("http")) return foto;
-  return `${import.meta.env.VITE_API_URL}${foto}`;
+
+  // Si la BD guarda "empleado_1.jpg"
+  return `${import.meta.env.VITE_API_URL}/fotos/${foto}`;
 };
+
 
 export default function ResumenEmpleado({ empleadoId }) {
   const { empleadoActual, cargarEmpleado } = useEmpleadosStore();

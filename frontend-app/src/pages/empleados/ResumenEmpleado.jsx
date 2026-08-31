404b3f47f6414e26a579cd8eebd057447bb4f0f0
victorxixes <victor.tomas.c@gmail.com>
2026-08-31 15:02:31 +0200

    Update ResumenEmpleado.jsx

--- frontend-app/src/pages/empleados/ResumenEmpleado.jsx
+++ frontend-app/src/pages/empleados/ResumenEmpleado.jsx
@@ -20,15 +20,15 @@ export default function ResumenEmpleado({ empleadoId }) {
   useEffect(() => {
     cargarEmpleado(empleadoId);
 
-    fetch(`${import.meta.env.VITE_API_URL}/empleados/departamentos`)
+    fetch(`${import.meta.env.VITE_API_URL}/maestros/departamentos`)
       .then((r) => r.json())
       .then(setDepartamentos);
 
-    fetch(`${import.meta.env.VITE_API_URL}/empleados/secciones`)
+    fetch(`${import.meta.env.VITE_API_URL}/maestros/secciones`)
       .then((r) => r.json())
       .then(setSecciones);
 
-    fetch(`${import.meta.env.VITE_API_URL}/empleados/cargos`)
+    fetch(`${import.meta.env.VITE_API_URL}/maestros/cargos`)
       .then((r) => r.json())
       .then(setCargos);
   }, [empleadoId]);

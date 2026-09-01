2d7d0aea2e2877f65d5ff9321c6f5bb35b8c75ac
victorxixes <victor.tomas.c@gmail.com>
2026-09-01 12:04:55 +0200

    Update service.py

--- backend/app/empleados/service.py
+++ backend/app/empleados/service.py
@@ -12,7 +12,7 @@ pwd_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
 # ---------------------------------------------------------
 def crear_empleado(db: Session, data: EmpleadoCreate):
 
-hashed_password = pwd_context.hash(data.password[:72])
+hashed_password = pwd_context.hash(data.password[:72]) if data.password else None
 
     empleado = Empleado(
         nombre=data.nombre,

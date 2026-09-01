58d8cbbd4884a2a03ed857f02d0b46a8e69a9d4c
victorxixes <victor.tomas.c@gmail.com>
2026-09-01 14:27:58 +0200

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,9 +4,6 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL no está definida")
-
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True

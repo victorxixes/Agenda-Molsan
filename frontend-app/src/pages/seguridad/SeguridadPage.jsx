0e62566a0a2c3836422fc1ba6fedc4a5ed046311
victorxixes <victor.tomas.c@gmail.com>
2026-09-01 22:39:27 +0200

    Update SeguridadPage.jsx

--- frontend-app/src/pages/seguridad/SeguridadPage.jsx
+++ frontend-app/src/pages/seguridad/SeguridadPage.jsx
@@ -1,22 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+
 import RolesList from "./RolesList";
 import RolForm from "./RolForm";
-import RolDetalle from "./RolDetalle";
-import ModulosRol from "./ModulosRol";
 import PermisosRol from "./PermisosRol";
+import ModulosRol from "./ModulosRol";
 import Auditoria from "./Auditoria";
 import EventosSeguridad from "./EventosSeguridad";
 
 export default function SeguridadPage() {
   return (
-    <Routes>
-      <Route path="roles" element={<RolesList />} />
-      <Route path="roles/nuevo" element={<RolForm />} />
-      <Route path="roles/:id" element={<RolDetalle />} />
-      <Route path="roles/:id/modulos" element={<ModulosRol />} />
-      <Route path="roles/:id/permisos" element={<PermisosRol />} />
+    <div className="p-6 space-y-10">
+      <Routes>
 
-      <Route path="auditoria" element={<Auditoria />} />
-      <Route path="eventos" element={<EventosSeguridad />} />
-    </Routes>
+        {/* ROLES */}
+        <Route path="roles" element={<RolesList />} />
+        <Route path="roles/nuevo" element={<RolForm />} />
+        <Route path="roles/:id" element={<RolForm />} />
+        <Route path="roles/:id/permisos" element={<PermisosRol />} />
+        <Route path="roles/:id/modulos" element={<ModulosRol />} />
+
+        {/* AUDITORÍA */}
+        <Route path="auditoria" element={<Auditoria />} />
+
+        {/* EVENTOS */}
+        <Route path="eventos" element={<EventosSeguridad />} />
+
+      </Routes>
+    </div>
   );
 }

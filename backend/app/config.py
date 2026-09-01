7d45349d3da0db6f083c53cd92c5d3103eb2a372
victorxixes <victor.tomas.c@gmail.com>
2026-09-01 10:31:16 +0200

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,15 +7,9 @@ class Settings:
     PROJECT_NAME: str = os.getenv("PROJECT_NAME", "Agenda Intranet")
 
     # -------------------------
-    # DATABASE
+    # DATABASE (SQLite fijo)
     # -------------------------
-    raw_url = os.getenv("DATABASE_URL", "sqlite:///./app.db")
-
-    # Render usa postgres:// pero SQLAlchemy exige postgresql://
-    if raw_url.startswith("postgres://"):
-        raw_url = raw_url.replace("postgres://", "postgresql://", 1)
-
-    DATABASE_URL: str = raw_url
+    DATABASE_URL: str = "sqlite:///./molsan.db"
 
     # -------------------------
     # JWT
@@ -31,3 +25,4 @@ class Settings:
 
 
 settings = Settings()
+

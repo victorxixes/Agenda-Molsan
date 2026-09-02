5a5a39c10ba85d7bd017f18dc07aa99eeab79e29
victorxixes <victor.tomas.c@gmail.com>
2026-09-02 09:15:35 +0200

    Update models.py

--- backend/app/empleados/models.py
+++ backend/app/empleados/models.py
@@ -60,3 +60,31 @@ class Empleado(Base):
     permisos_modulo = Column(JSONB, nullable=True)
 
     creado_en = Column(DateTime, default=datetime.utcnow)
+
+class Departamento(Base):
+    __tablename__ = "departamentos"
+    __allow_unmapped__ = True
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(150), unique=True, nullable=False)
+    descripcion = Column(String(255), nullable=True)   # 🔥 añadir esto
+
+    empleados = relationship("Empleado", back_populates="departamento")
+
+class Seccion(Base):
+    __tablename__ = "secciones"
+    __allow_unmapped__ = True
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(150), unique=True, nullable=False)
+
+    empleados = relationship("Empleado", back_populates="seccion")
+
+class Cargo(Base):
+    __tablename__ = "cargos"
+    __allow_unmapped__ = True
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String(150), unique=True, nullable=False)
+
+    empleados = relationship("Empleado", back_populates="cargo")

889c259f3e7233bfaab3bdee5e9842e3a5ebc659
victorxixes <victor.tomas.c@gmail.com>
2026-09-02 12:34:44 +0200

    Update models.py

--- backend/app/empleados/models.py
+++ backend/app/empleados/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Boolean, JSON
-from app.database import Base
+from backend.app.database import Base
 
 class Empleado(Base):
     __tablename__ = "empleados"

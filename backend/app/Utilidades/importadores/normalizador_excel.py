7a84d9a3120d804d43aa0fcabbc898dc7664952c
victorxixes <victor.tomas.c@gmail.com>
2026-09-02 15:20:36 +0200

    Update normalizador_excel.py

--- backend/app/Utilidades/importadores/normalizador_excel.py
+++ backend/app/Utilidades/importadores/normalizador_excel.py
@@ -3,24 +3,18 @@ from io import BytesIO, StringIO
 
 def normalizar_excel(file):
     try:
-        # Leer el archivo en memoria
         contenido = file.file.read()
         buffer = BytesIO(contenido)
 
-        # Primer intento con openpyxl
         df = pd.read_excel(buffer, engine="openpyxl", dtype=str)
 
     except Exception:
         try:
-            # Reiniciar puntero
             buffer.seek(0)
-
-            # Segundo intento con xlrd
             df = pd.read_excel(buffer, engine="xlrd", dtype=str)
         except Exception:
             return None, "Archivo corrupto o formato no soportado"
 
-    # Convertir a CSV interno
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=False)
     csv_buffer.seek(0)

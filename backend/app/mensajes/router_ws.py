07f98d1cc11e2251b2e9ef59a413f81a2251be8b
victorxixes <victor.tomas.c@gmail.com>
2026-09-03 08:02:54 +0200

    Update router_ws.py

--- backend/app/mensajes/router_ws.py
+++ backend/app/mensajes/router_ws.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from backend.app.mensajes.ws_manager import manager
 
-router = APIRouter(prefix="/ws/mensajes", tags=["Mensajes WS"])
+router = APIRouter()
 
-@router.websocket("/{empleado_id}")
+@router.websocket("/ws/mensajes/{empleado_id}")
 async def mensajes_ws(websocket: WebSocket, empleado_id: int):
     await manager.connect(websocket, empleado_id)
     print(f"[WS-MSG] Conectado: {empleado_id}")
@@ -12,16 +12,13 @@ async def mensajes_ws(websocket: WebSocket, empleado_id: int):
         while True:
             data = await websocket.receive_json()
 
-            # Si el front envía un mensaje:
             if data.get("tipo") == "mensaje":
                 remitente_id = empleado_id
                 destinatario_id = data.get("destinatario_id")
                 contenido = data.get("contenido")
 
-                # ⭐ Enviar mensaje en tiempo real si el destinatario está conectado
                 await manager.enviar_mensaje_ws(remitente_id, destinatario_id, contenido)
 
-                # ⭐ Enviar notificación de nuevo mensaje
                 await manager.enviar_notificacion(destinatario_id, {
                     "tipo": "nuevo_mensaje",
                     "de": remitente_id,

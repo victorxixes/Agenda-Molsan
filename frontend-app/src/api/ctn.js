9a898fa4d02f782b1df4fc9df43f21c5b7faa504
victorxixes <victor.tomas.c@gmail.com>
2026-09-04 15:22:16 +0200

    Update ctn.js

--- frontend-app/src/api/ctn.js
+++ frontend-app/src/api/ctn.js
@@ -1,7 +1,20 @@
 import axios from "./axios";
 
-export const listarNotarias = (search = "") =>
-  axios.get("/ctn", { params: { search } });
+export const listarNotarias = ({
+  provincia,
+  municipio,
+  vc,
+  apoderado,
+  q,
+  page = 1,
+  page_size = 50,
+} = {}) =>
+  axios.get("/ctn/notarias", {
+    params: { provincia, municipio, vc, apoderado, q, page, page_size },
+  });
 
 export const obtenerNotaria = (id) =>
-  axios.get(`/ctn/${id}`);
+  axios.get(`/ctn/notarias/${id}`);
+
+export const obtenerFirmasNotaria = (id) =>
+  axios.get(`/ctn/notarias/${id}/firmas`);

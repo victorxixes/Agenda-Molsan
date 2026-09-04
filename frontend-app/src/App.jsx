bfd56dc7766c767af7757749f5a393bde9352303
victorxixes <victor.tomas.c@gmail.com>
2026-09-04 15:18:31 +0200

    Update App.jsx

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -1,4 +1,8 @@
 <Route path="/ctn" element={<Ctn />} />
 <Route path="/ctn/:id" element={<CtnDetallePage />} />
 <Route path="/logs" element={<Logs />} />
+<Route path="/herramientas" element={<Herramientas />} />
+<Route path="/herramientas/importar-ctn" element={<ImportarCTN />} />
+<Route path="/herramientas/importar-apoderados" element={<ImportarApoderados />} />
+<Route path="/herramientas/utilidades" element={<Utilidades />} />
 

0766913575909adf77d8e1c4d59d6ee194166ff4
victorxixes <victor.tomas.c@gmail.com>
2026-09-04 15:22:27 +0200

    Update ctnStore.js

--- frontend-app/src/store/ctnStore.js
+++ frontend-app/src/store/ctnStore.js
@@ -2,19 +2,34 @@ import { create } from "zustand";
 import * as api from "../api/ctn";
 
 export const useCtnStore = create((set) => ({
-  notarias: [],
+  items: [],
+  total: 0,
+  page: 1,
+  page_size: 50,
   notaria: null,
+  firmas: null,
   loading: false,
 
-  cargarNotarias: async (search = "") => {
+  cargarNotarias: async (filtros = {}) => {
     set({ loading: true });
-    const res = await api.listarNotarias(search);
-    set({ notarias: res.data, loading: false });
+    const res = await api.listarNotarias(filtros);
+    set({
+      items: res.data.items,
+      total: res.data.total,
+      page: res.data.page,
+      page_size: res.data.page_size,
+      loading: false,
+    });
   },
 
   cargarNotaria: async (id) => {
     set({ loading: true });
     const res = await api.obtenerNotaria(id);
     set({ notaria: res.data, loading: false });
+  },
+
+  cargarFirmasNotaria: async (id) => {
+    const res = await api.obtenerFirmasNotaria(id);
+    set({ firmas: res.data });
   },
 }));

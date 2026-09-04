d61c37c0a34c0ce1886ebe2b53c00a2c3771cbb9
victorxixes <victor.tomas.c@gmail.com>
2026-09-04 19:25:09 +0200

    Update App.jsx

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -1,14 +1,24 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
-import Ctn from "./pages/ctn/Ctn";
-import CtnDetallePage from "./pages/ctn/CtnDetallePage";
-import Logs from "./pages/logs/Logs";
-import Herramientas from "./pages/herramientas/Herramientas";
-import ImportarCTN from "./pages/herramientas/ImportarCTN";
-import Utilidades from "./pages/herramientas/Utilidades";
-import Seguridad from "./pages/seguridad/Seguridad";
-import EmpleadosModulo2026 from "./pages/Empleados/EmpleadosModulo2026";
+
+/* CTN */
+import Ctn from "./pages/ctn/Ctn.jsx";
+import CtnDetallePage from "./pages/ctn/CtnDetallePage.jsx";
+
+/* LOGS */
+import Logs from "./pages/logs/Logs.jsx";
+
+/* HERRAMIENTAS */
+import Herramientas from "./pages/herramientas/Herramientas.jsx";
+import ImportarCTN from "./pages/herramientas/ImportarCTN.jsx";
+import Utilidades from "./pages/herramientas/Utilidades.jsx";
+
+/* SEGURIDAD */
+import Seguridad from "./pages/seguridad/Seguridad.jsx";
+
+/* EMPLEADOS */
+import EmpleadosModulo2026 from "./pages/empleados/EmpleadosModulo2026.jsx";
 
 export default function App() {
   return (
@@ -22,11 +32,15 @@ export default function App() {
       {/* MÓDULOS */}
       <Route path="/ctn" element={<Ctn />} />
       <Route path="/ctn/:id" element={<CtnDetallePage />} />
+
       <Route path="/logs" element={<Logs />} />
+
       <Route path="/herramientas" element={<Herramientas />} />
       <Route path="/herramientas/importar-ctn" element={<ImportarCTN />} />
       <Route path="/herramientas/utilidades" element={<Utilidades />} />
+
       <Route path="/seguridad" element={<Seguridad />} />
+
       <Route path="/panel/empleados" element={<EmpleadosModulo2026 />} />
     </Routes>
   );

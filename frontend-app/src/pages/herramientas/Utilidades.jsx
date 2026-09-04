89b3114614b54acac6fe7d774e4c9248e4c99be4
victorxixes <victor.tomas.c@gmail.com>
2026-09-04 19:21:35 +0200

    Create Utilidades.jsx

---
+++ frontend-app/src/pages/herramientas/Utilidades.jsx
@@ -0,0 +1,8 @@
+export default function Utilidades() {
+  return (
+    <div className="card-sj">
+      <h1>Herramientas / Utilidades SJ‑2026</h1>
+      <p>Módulo de utilidades cargado correctamente.</p>
+    </div>
+  );
+}

1cec6643bee48ad935f70beac9b52fc92f94f625
victorxixes <victor.tomas.c@gmail.com>
2026-09-04 14:43:47 +0200

    Update authStore.js

--- frontend-app/src/store/authStore.js
+++ frontend-app/src/store/authStore.js
@@ -1,1 +1,18 @@
+import { create } from "zustand";
+import { login } from "../api/auth";
+
+export const useAuthStore = create((set) => ({
+  empleado: null,
+
+  iniciarSesion: async (usuario, password) => {
+    const res = await login(usuario, password);
+
+    if (res.data.status === "ok") {
+      set({ empleado: res.data.empleado });
+      return true;
+    }
+
+    return false;
+  },
+}));
 

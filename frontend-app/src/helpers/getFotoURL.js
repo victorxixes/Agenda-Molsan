07fca744560fd9443f2b0becc2556cb7e76237e2
victorxixes <victor.tomas.c@gmail.com>
2026-09-04 13:49:05 +0200

    Delete frontend-app/src/helpers directory

--- frontend-app/src/helpers/getFotoURL.js
+++
@@ -1,8 +0,0 @@
-export const getFotoURL = (foto) => {
-  if (!foto || foto.trim() === "") {
-    return `${import.meta.env.VITE_API_URL}/api/fotos/default.jpg`;
-  }
-
-  return `${import.meta.env.VITE_API_URL}/api/fotos/${foto}`;
-};
-

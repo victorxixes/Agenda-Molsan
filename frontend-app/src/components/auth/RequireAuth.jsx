92be5cacc9eedeeaf5bbd6e58ecbf0e2630250c4
victorxixes <victor.tomas.c@gmail.com>
2026-09-05 18:13:14 +0200

    Update RequireAuth.jsx

--- frontend-app/src/components/auth/RequireAuth.jsx
+++ frontend-app/src/components/auth/RequireAuth.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuthStore } from "@/store/authStore";
+import { useAuthStore } from "../../store/authStore";
 
 export default function RequireAuth({ children }) {
   const token = useAuthStore((s) => s.token);

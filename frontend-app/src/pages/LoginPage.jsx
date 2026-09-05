b3a3e3fdcbb252c19accf077ce17acb2e55c42c5
victorxixes <victor.tomas.c@gmail.com>
2026-09-05 18:21:47 +0200

    Fix import path for useAuthStore in LoginPage

--- frontend-app/src/pages/LoginPage.jsx
+++ frontend-app/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuthStore } from "../../store/authStore";
+import { useAuthStore } from "../store/authStore";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {

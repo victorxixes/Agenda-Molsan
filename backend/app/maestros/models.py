653d8c052d099dd2cfb0733a52cbe1bf81b7c000
victorxixes <victor.tomas.c@gmail.com>
2026-09-05 08:11:19 +0200

    Update models.py

--- backend/app/maestros/models.py
+++ backend/app/maestros/models.py
@@ -3,24 +3,15 @@ from backend.app.database import Base
 
 class Departamento(Base):
     __tablename__ = "departamentos"
-
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True)
 
 class Seccion(Base):
     __tablename__ = "secciones"
-
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True)
 
 class Cargo(Base):
     __tablename__ = "cargos"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, unique=True)
-
-class Rol(Base):
-    __tablename__ = "roles"
-
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True)

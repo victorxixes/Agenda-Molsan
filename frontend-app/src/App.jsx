8079246d7940b6b1df7e228011cafefcdf231e05
victorxixes <victor.tomas.c@gmail.com>
2026-09-07 16:23:27 +0200

    Update App.jsx

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -41,6 +41,8 @@ import Mensajes from "./pages/mensajes/Mensajes.jsx";
 import Herramientas from "./pages/herramientas/Herramientas.jsx";
 import ImportarCTN from "./pages/herramientas/ImportarCTN.jsx";
 import Utilidades from "./pages/herramientas/Utilidades.jsx";
+import CrearNoticia from "./pages/herramientas/CrearNoticia.jsx";
+import SubirDocumento from "./pages/herramientas/SubirDocumento.jsx";
 
 
 /* LOGS */

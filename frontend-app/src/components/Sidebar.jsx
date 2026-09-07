af13c35964408f207c68255ad65ed6e380374b08
victorxixes <victor.tomas.c@gmail.com>
2026-09-07 11:24:30 +0200

    Update Sidebar.jsx

--- frontend-app/src/components/Sidebar.jsx
+++ frontend-app/src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ export default function Sidebar() {
 
   return (
     <aside className="w-64 bg-white border-r shadow-sm p-4 space-y-4">
-      <h2 className="text-xl font-bold mb-4">ERP Molsan 2026</h2>
+      <h2 className="text-xl font-bold mb-4">Agenda Molsan</h2>
 
       <nav className="space-y-2">
 

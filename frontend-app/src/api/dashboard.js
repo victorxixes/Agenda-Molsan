f18266f243d4a43d2c409d9ebd92ca5914059b10
victorxixes <victor.tomas.c@gmail.com>
2026-09-07 13:36:20 +0200

    Update dashboard.js

--- frontend-app/src/api/dashboard.js
+++ frontend-app/src/api/dashboard.js
@@ -3,5 +3,3 @@ import axios from "./axios";
 export const obtenerDashboard = () =>
   axios.get("/dashboard");
 
-export const obtenerDashboardExtendido = () =>
-  axios.get("/dashboard/extendido");

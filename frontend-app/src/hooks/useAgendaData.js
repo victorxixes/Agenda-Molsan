6a799f4188ff01bdd4d312ad6346ff4792d13cd7
victorxixes <victor.tomas.c@gmail.com>
2026-09-08 16:58:32 +0200

    Update useAgendaData.js

--- frontend-app/src/hooks/useAgendaData.js
+++ frontend-app/src/hooks/useAgendaData.js
@@ -2,28 +2,24 @@ import { useEffect } from "react";
 import { useAgendaStore } from "../store/agendaStore";
 import { useEmpleadosStore } from "../store/empleadosStore";
 import { useNotariasStore } from "../store/notariasStore";
-import { useAgendaWS } from "./useAgendaWS";
 
 export function useAgendaData(year, month) {
   const { citas, cargarMes } = useAgendaStore();
   const { apoderados, cargarApoderados } = useEmpleadosStore();
-
-  // ⭐ Nombres correctos del store
   const { notarias, cargarNotarias } = useNotariasStore();
 
-  // WebSocket blindado
-  //useAgendaWS(1);
+  // WebSocket desactivado temporalmente
+  // useAgendaWS(1);
 
-  // Cargar todo en paralelo
   useEffect(() => {
     cargarMes(year, month);
     cargarApoderados();
-    cargarNotarias();   // ⭐ nombre correcto
+    cargarNotarias();
   }, [year, month]);
 
   return {
     citas,
     apoderados,
-    notarias,           // ⭐ nombre correcto
+    notarias,
   };
 }

8446ec2913840ca0af673e4e88e86907e939ecb0
victorxixes <victor.tomas.c@gmail.com>
2026-09-08 10:32:07 +0200

    Update empleadosStore.js

--- frontend-app/src/store/empleadosStore.js
+++ frontend-app/src/store/empleadosStore.js
@@ -1,53 +1,55 @@
 import { create } from "zustand";
-import * as api from "../api/empleados";
+import {
+  listarEmpleados,
+  obtenerEmpleado,
+  crearEmpleado,
+  editarEmpleado,
+  eliminarEmpleado,
+  listarApoderados,
+} from "../api/empleados";
 
 export const useEmpleadosStore = create((set, get) => ({
   empleados: [],
-  empleadoActual: null,
-  modulosVisibles: [],
-  permisosModulo: {},
+  apoderados: [],
+  cargando: false,
 
   cargarEmpleados: async () => {
-    const res = await api.listarEmpleados();
-    set({ empleados: res.data });
+    set({ cargando: true });
+
+    const res = await listarEmpleados();
+    const lista = Array.isArray(res.data) ? res.data : [];
+
+    set({ empleados: lista, cargando: false });
   },
 
-  buscar: async (q, activo) => {
-    const res = await api.buscarEmpleados(q, activo);
-    set({ empleados: res.data });
+  cargarApoderados: async () => {
+    set({ cargando: true });
+
+    const res = await listarApoderados();
+    const lista = Array.isArray(res.data) ? res.data : [];
+
+    set({ apoderados: lista, cargando: false });
   },
 
   obtener: async (id) => {
-    const res = await api.obtenerEmpleado(id);
-    set({ empleadoActual: res.data });
+    const res = await obtenerEmpleado(id);
+    return res.data || null;
   },
 
-  crear: async (data) => {
-    const res = await api.crearEmpleado(data);
+  crear: async (payload) => {
+    const res = await crearEmpleado(payload);
     await get().cargarEmpleados();
-    return res.data;
+    return res.data || null;
   },
 
-  editar: async (id, data) => {
-    const res = await api.editarEmpleado(id, data);
+  editar: async (id, payload) => {
+    const res = await editarEmpleado(id, payload);
     await get().cargarEmpleados();
-    return res.data;
+    return res.data || null;
   },
 
   eliminar: async (id) => {
-    await api.eliminarEmpleado(id);
+    await eliminarEmpleado(id);
     await get().cargarEmpleados();
-  },
-
-  actualizarModulos: async (id, modulos) => {
-    const res = await api.actualizarModulosVisibles(id, modulos);
-    set({ modulosVisibles: res.data.modulos_visibles_list });
-    return res.data;
-  },
-
-  actualizarPermisos: async (id, permisos) => {
-    const res = await api.actualizarPermisosModulo(id, permisos);
-    set({ permisosModulo: res.data.permisos_modulo_dict });
-    return res.data;
   },
 }));

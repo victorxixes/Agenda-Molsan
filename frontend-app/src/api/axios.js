958c2ef5ad47b7e59ea35fc55c5c253657a8d57c
victorxixes <victor.tomas.c@gmail.com>
2026-09-10 16:10:03 +0200

    Update axios.js

--- frontend-app/src/api/axios.js
+++ frontend-app/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
  
 const instance = axios.create({
   // ✔ baseURL correcta: tu backend expone /api, aquí lo añadimos una sola vez
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 

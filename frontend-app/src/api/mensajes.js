d0b122ea323b0559bf698d7c00e92eb36786779f
victorxixes <victor.tomas.c@gmail.com>
2026-09-10 08:29:10 +0200

    Update mensajes.js

--- frontend-app/src/api/mensajes.js
+++ frontend-app/src/api/mensajes.js
@@ -1,22 +1,22 @@
 import axios from "./axios";
 
 export const obtenerConectados = () =>
-  axios.get("/mensajes/conectados");
+  axios.get("/api/mensajes/conectados");
 
 export const enviarMensajeREST = (data) =>
-  axios.post("/mensajes", data);
+  axios.post("/api/mensajes", data);
 
 export const subirArchivo = (file) => {
   const formData = new FormData();
   formData.append("file", file);
-  return axios.post("/mensajes/upload", formData);
+  return axios.post("/api/mensajes/upload", formData);
 };
 
 export const obtenerConversacion = (usuarioId, otroId) =>
-  axios.get(`/mensajes/${usuarioId}/${otroId}`);
+  axios.get(`/api/mensajes/${usuarioId}/${otroId}`);
 
 export const marcarLeido = (mensajeId) =>
-  axios.put(`/mensajes/leido/${mensajeId}`);
+  axios.put(`/api/mensajes/leido/${mensajeId}`);
 
 export const marcarConversacionLeida = (usuarioId, otroId) =>
-  axios.put(`/mensajes/leido/conversacion/${usuarioId}/${otroId}`);
+  axios.put(`/api/mensajes/leido/conversacion/${usuarioId}/${otroId}`);

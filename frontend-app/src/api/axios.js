81cefe17f291e980a6c10c746eab4246f7e5dc92
victorxixes <victor.tomas.c@gmail.com>
2026-09-10 11:54:55 +0200

    Update axios.js

--- frontend-app/src/api/axios.js
+++ frontend-app/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: false,
+  withCredentials: true,
 });
 
 // Interceptor de request

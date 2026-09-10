167ba8c778e811d5314868b60d0ad90d26d7cbc2
victorxixes <victor.tomas.c@gmail.com>
2026-09-10 15:30:45 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import os
-
+ 
 # ---------------------------------------------------------
 # APP
 # ---------------------------------------------------------

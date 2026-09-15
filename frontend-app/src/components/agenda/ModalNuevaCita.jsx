d26fdb6f777a66d8b7f10758c800fc3a70ceefc4
victorxixes <victor.tomas.c@gmail.com>
2026-09-15 13:43:11 +0200

    Update ModalNuevaCita.jsx

--- frontend-app/src/components/agenda/ModalNuevaCita.jsx
+++ frontend-app/src/components/agenda/ModalNuevaCita.jsx
@@ -320,13 +320,10 @@ useEffect(() => {
     {modo === "crear" ? "Crear cita" : "Guardar cambios"}
   </button>
 </div>
+        </div> {/* cierre del grid interno */}
 
-</div> {/* cierre del grid interno */}
+      </div> {/* cierre del contenedor del modal */}
 
-</div> {/* cierre del contenedor del modal */}
-
-</div> {/* cierre del overlay */}
-
-); /* cierre del return */
-
-} /* cierre del componente */
+    </div> {/* cierre del overlay */}
+  );
+}

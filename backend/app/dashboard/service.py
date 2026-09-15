0bf8b986f0708a6af67301d5f140881adcffe667
victorxixes <victor.tomas.c@gmail.com>
2026-09-15 12:54:56 +0200

    Update service.py

--- backend/app/dashboard/service.py
+++ backend/app/dashboard/service.py
@@ -38,7 +38,7 @@ def obtener_dashboard(db: Session):
             "fecha": str(c.fecha),
             "notario": notario_nombre,
             "apoderado": c.apoderado_s,
-            "tipo_firma": "Videoconferencia" if c.vc == "SI" else "Presencial"
+            "tipo_firma": "Videoconferencia" if c.vc == "SI" else "Presencial",
             "hora_inicio": str(c.hora_inicio),
             "hora_fin": str(c.hora_fin)
         })

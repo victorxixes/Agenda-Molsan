eef8e174e46f5d8ebc041ebaf3133bea307bd72c
victorxixes <victor.tomas.c@gmail.com>
2026-09-16 19:52:54 +0200

    Update auth.js

--- frontend-app/src/api/auth.js
+++ frontend-app/src/api/auth.js
@@ -1,8 +1,13 @@
 import axios from "./axios";
 
+/**
+ * API Auth — Versión SJ‑2026 Premium
+ * Gestiona:
+ * - Inicio de sesión
+ */
+
 export const login = (usuario, password) =>
   axios.post("/auth/login", {
     usuario,
     password,
   });
-

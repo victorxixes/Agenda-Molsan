f210e6aca312d9c07fcaafcd54611d039ba91e34
victorxixes <victor.tomas.c@gmail.com>
2026-09-16 19:55:32 +0200

    Update dashboard.js

--- frontend-app/src/api/dashboard.js
+++ frontend-app/src/api/dashboard.js
@@ -1,5 +1,9 @@
 import axios from "./axios";
 
+/**
+ * API Dashboard — Versión SJ‑2026 Premium
+ * Obtiene datos agregados del sistema.
+ */
+
 export const obtenerDashboard = () =>
   axios.get("/dashboard");
-

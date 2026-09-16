5f27bb9be25f8b6574edfd50872e62fe397f9ee8
victorxixes <victor.tomas.c@gmail.com>
2026-09-16 19:55:46 +0200

    Update herramientas.js

--- frontend-app/src/api/herramientas.js
+++ frontend-app/src/api/herramientas.js
@@ -1,5 +1,10 @@
 import axios from "./axios";
 
+/**
+ * API Herramientas — Versión SJ‑2026 Premium
+ * Importación de CTN (Excel)
+ */
+
 export const importarCTN = (file) => {
   const form = new FormData();
   form.append("fichero", file);

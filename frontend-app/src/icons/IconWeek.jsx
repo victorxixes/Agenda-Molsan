ce4de2cc0a03241f6430317133894bee3bea5552
victorxixes <victor.tomas.c@gmail.com>
2026-09-17 09:07:14 +0200

    Create IconWeek.jsx

---
+++ frontend-app/src/icons/IconWeek.jsx
@@ -0,0 +1,15 @@
+export default function IconWeek(props) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <line x1="3" y1="6" x2="21" y2="6" />
+      <line x1="3" y1="12" x2="21" y2="12" />
+      <line x1="3" y1="18" x2="21" y2="18" />
+    </svg>
+  );
+}

1aa032577fccb97906263238268b6e2c91ff50b7
victorxixes <victor.tomas.c@gmail.com>
2026-09-17 09:07:23 +0200

    Create IconMonth.jsx

---
+++ frontend-app/src/icons/IconMonth.jsx
@@ -0,0 +1,14 @@
+export default function IconMonth(props) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+    >
+      <rect x="4" y="4" width="16" height="16" rx="2" />
+      <line x1="4" y1="10" x2="20" y2="10" />
+    </svg>
+  );
+}

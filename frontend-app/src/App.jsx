9414788a4e3f101a2cb0f0940b70407a09ff239f
victorxixes <victor.tomas.c@gmail.com>
2026-09-18 10:05:48 +0200

    Update App.jsx

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -131,6 +131,7 @@ export default function App() {
           <Route path="empleados/:id/editar" element={<EmpleadoEditar />} />
           
           {/* INTRANET */}
+          console.log("CARGANDO INTRANET:", Intranet);
           <Route path="intranet" element={<Intranet />} />
 
           {/* MENSAJES */}

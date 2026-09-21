7c9bd70f5e2a685e24b6c53c216571e4eae706bf
victorxixes <victor.tomas.c@gmail.com>
2026-09-21 15:04:29 +0200

    Update Sidebar.jsx

--- frontend-app/src/components/Sidebar.jsx
+++ frontend-app/src/components/Sidebar.jsx
@@ -105,34 +105,25 @@ export default function Sidebar() {
         `}
         onMouseLeave={() => !fixed && setCollapsed(true)}
       >
- {/* MI PERFIL */}
+
+        {/* MI PERFIL */}
 {!collapsed && (
   <div className="mt-4">
     <button
       onClick={() => useAuthStore.getState().setPerfilModal(empleado.id)}
       className="
-        flex items-center gap-3 px-3 py-2 rounded-xl w-full
+        flex items-center gap-4 px-3 py-2 rounded-xl w-full
         text-white hover:bg-white/10 transition-all duration-300
         group
       "
     >
-      <div
-        className="
-          w-10 h-10 flex items-center justify-center rounded-xl
-          bg-white/10 text-white/70
-          group-hover:bg-white/20 group-hover:text-white group-hover:scale-105
-          transition-all duration-300
-        "
-      >
-        <svg className="w-5 h-5">
-          <use href="/icons/icons.svg#user" />
-        </svg>
-      </div>
+      <IconRound name="user" active={false} />
 
       <span className="font-medium">Mi perfil</span>
     </button>
   </div>
 )}
+
 
         {/* HEADER */}
         {!collapsed && (

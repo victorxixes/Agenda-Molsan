d875d2215cb623018f4161e1b0ba2b089edcc953
victorxixes <victor.tomas.c@gmail.com>
2026-09-22 10:55:30 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -182,5 +182,5 @@ app.include_router(mensajes_router, prefix="/api")
 
 # Utilidades
 app.include_router(utilidades_router, prefix="/api")
-app.include_router(informes_router)
+app.include_router(informes_router, prefix="/api")
 

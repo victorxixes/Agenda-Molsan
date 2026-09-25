b432500bf1a82758078519fdb49c600b211aca77
victorxixes <victor.tomas.c@gmail.com>
2026-09-25 11:09:55 +0200

    Refactor FastAPI application structure and imports

--- backend/app/main.py
+++ backend/app/main.py
@@ -118,6 +118,9 @@ from backend.app.dashboard.router import router as dashboard_router
 # Utilidades
 from backend.app.Utilidades.router import router as utilidades_router
 from backend.app.informes.router import router as informes_router
+from app.utilidades.router_import import router as utilidades_import_router
+
+
 
 # ============================================================
 # INCLUIR ROUTERS (orden correcto)
@@ -182,3 +185,5 @@ app.include_router(mensajes_router, prefix="/api")
 # Utilidades
 app.include_router(utilidades_router, prefix="/api")
 app.include_router(informes_router, prefix="/api")
+app.include_router(expedientes_import_router)
+
